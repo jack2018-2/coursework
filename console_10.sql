4d78da54db8859723746142d39a49884b7f634ff
jack2018-2 <32910367+jack2018-2@users.noreply.github.com>
2020-11-23 22:03:09 +0300

    added trigger and trigger procedure for: -counting volume of drink -counting the alcohol degree -searching for the cocktail base

--- console_10.sql
+++ console_10.sql
@@ -2,10 +2,10 @@ create type taste as enum ('spicy', 'salty', 'sweet', 'sour', 'bitter');
 
 create table ingredients
 (
-    "Id"        serial not null
+    "id"        serial not null
         constraint ingredients_pk
             primary key,
-    name        varchar(30),
+    name        varchar(50),
     degree      smallint,
     type        text,
     description text,
@@ -17,9 +17,9 @@ comment on column ingredients.type is 'По ER здесь должен быть 
 alter table ingredients
     owner to postgres;
 
-create table "Cocktails"
+create table "сocktails"
 (
-    "Id"        serial      not null
+    "id"        serial      not null
         constraint cocktails_pk
             primary key,
     name        varchar(40) not null,
@@ -35,14 +35,14 @@ create table "Cocktails"
             references ingredients
 );
 
-alter table "Cocktails"
+alter table "сocktails"
     owner to postgres;
 
 create table cocktails_ingridients
 (
     cocktail_id   integer
         constraint cocktail_id
-            references "Cocktails",
+            references "сocktails",
     ingridient_id integer
         constraint ingridient_id
             references ingredients,
@@ -54,10 +54,10 @@ alter table cocktails_ingridients
 
 create table tools
 (
-    "Id"        serial not null
+    "id"        serial not null
         constraint tools_pk
             primary key,
-    name        varchar(30),
+    name        varchar(50),
     description text,
     picture     bytea
 );
@@ -69,7 +69,7 @@ create table cocktails_tools
 (
     cocktail_id integer
         constraint cocktail_id
-            references "Cocktails",
+            references "сocktails",
     tool_id     integer
         constraint tool_id
             references tools
@@ -78,3 +78,50 @@ create table cocktails_tools
 alter table cocktails_tools
     owner to postgres;
 
+
+--drop trigger calculatevolume on cocktails;
+--drop function calculatevolume();
+
+create function calculatevolume() returns trigger
+    language plpgsql
+as
+$$
+declare
+    ing record;
+    deg record;
+    maxdeg int;
+    maxid int;
+begin
+    new.volume:=0;
+    FOR ing IN
+        SELECT volume FROM cocktails_ingridients WHERE cocktail_id = new.id
+    LOOP
+        new.volume = new.volume + ing.volume;
+    end loop;
+    new.degree:=0;
+    maxdeg = 0;
+    maxid = 0;
+    FOR deg IN
+        SELECT ingredients.id, degree, ci.volume FROM ingredients join cocktails_ingridients ci on ingredients.id = ci.ingridient_id where ci.cocktail_id = new.id
+    LOOP
+        if (deg.degree > maxdeg) then maxdeg=deg.degree; maxid = deg.id; end if;
+        new.degree = new.degree + deg.degree * deg.volume / new.volume;
+    end loop;
+    new.basis_id = maxid;
+    return new;
+end;
+$$;
+
+alter function calculatevolume() owner to postgres;
+
+create trigger calculatevolume
+    before insert or update
+    on cocktails
+    for each row
+    execute procedure calculatevolume();
+
+
+--insert into cocktails (name, degree) values ('pzdc', 15);
+--insert into cocktails_ingridients values (5, 1, 100);
+--insert into ingredients values (3, 'try');
+--update cocktails set receipt = 'на глаз';
